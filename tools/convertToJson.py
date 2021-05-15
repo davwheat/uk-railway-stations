c740656f572e968d42a3a2709ad095361523f774
David Wheatley <hi@davwheat.dev>
2021-05-15 23:51:23 +0100

    Update conversion script to work form any cwd

--- tools/convertToJson.py
+++ tools/convertToJson.py
@@ -1,4 +1,12 @@
+from pathlib import Path
 import pandas as pd
 
-df = pd.read_csv(r"../stations.csv")
-df.to_json(r"../stations.json", orient="records")
+currentDir = Path(__file__).parent.resolve()
+
+
+def getPath(path):
+    return currentDir.joinpath(path)
+
+
+df = pd.read_csv(getPath("../stations.csv"))
+df.to_json(getPath("../stations.json"), orient="records")
